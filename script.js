a1fd34e76a5e99f5b7b09e1688093a06dc8e5ce3
RKCapital0018 <kautuk0018@gmail.com>
2026-02-20 10:49:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,24 @@ interestRate.addEventListener("input", updateValues);
 loanTenure.addEventListener("input", updateValues);
 
 updateValues();
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText;
+
+        const increment = target / 100;
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
