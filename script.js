672f0cabd35d232b00e1fc2f09fe91a87d2a07f0
RKCapital0018 <kautuk0018@gmail.com>
2026-04-15 14:28:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ document.querySelectorAll(".card, .product, .hero-text").forEach(el => {
   el.classList.add("hidden");
   observer.observe(el);
 });
+let cart = [];
+
+function openVeg() {
+  document.getElementById("vegSection").style.display = "flex";
+}
+
+function closeVeg() {
+  document.getElementById("vegSection").style.display = "none";
+}
+
+function addToCart(name, price, id) {
+  let qty = document.getElementById(id).value;
+
+  if(qty > 0){
+    cart.push({
+      name: name,
+      price: price,
+      quantity: qty
+    });
+
+    alert(name + " added to cart!");
+  } else {
+    alert("Please select quantity");
+  }
+}
